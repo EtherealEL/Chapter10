6fd81d14dae4abc63069183f4a5d1fbd0ea3eb68
EtherealEL <mjone148@aum.edu>
2026-03-09 16:14:30 -0500

    Add files via upload

---
+++ Practice70_E_Wallet_Top_Up_and_Spending_Guard.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class EWallet
+{
+private:
+	int balance;
+
+public:
+	EWallet()
+	{
+		balance = 0;
+	}
+	void set_balance(int new_balance)
+	{
+		if (new_balance >= 0)
+		{
+			balance = new_balance;
+		}
+		else
+		{
+			cout << "!! Invalid Balance !!" << endl;
+		}
+	}
+	
+
+	int get_balance()
+	{
+		return balance;
+	}
+
+	void spend(int amount)
+	{
+		if (amount > 0 && amount <= balance)
+		{
+			balance -= amount;
+		}
+		else
+		{
+			cout << "Payment DECLINED!" << endl;
+		}
+	}
+
+
+};
+
+
+
+
+
+
+int main()
+{
+	EWallet wallet;
+
+	int initial_balance;
+	int spend_amount;
+
+	cin >> initial_balance;
+	cin >> spend_amount;
+
+	wallet.set_balance(initial_balance);
+	wallet.spend(spend_amount);
+
+	cout << "Final balance: " << wallet.get_balance() << endl;
+
+	return 0;
+
+}
+
